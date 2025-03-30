6d09ceac83e035e13b7aa63c91a5d50005bf6af3
akshar <akshargothi70@gmail.com>
2025-03-29 20:53:12 -0700

    Change DB file

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/ai-powered-mock-interview', {
+    await mongoose.connect(process.env.DATABASE_URL , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
